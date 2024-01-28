7fcb8aa7d753b96e52ca97a83256dd873646aaeb
Maryssa Johnson <98675705+MaryssaJohnson@users.noreply.github.com>
2024-01-28 05:21:37 -0600

    Add files via upload

---
+++ MenuDrivenProject.cpp
@@ -0,0 +1,43 @@
+//Maryssa Johnson
+//Dr.Nina Javaher
+//1/29/2024
+//Menu Driven Project
+
+
+#include <iostream>    // Needed for cin and cout
+#include <string>      // Needed for string operations
+#include <iomanip>     // Needed for setfill and setw
+#include <cmath>       // Needed for pow and other math functions
+
+using namespace std;
+
+int main() {
+    int choice;          // To hold the user's menu choice
+    string name;         // To hold user's name
+    int age;             // To hold user's age
+}  
+
+do {
+    //Display the menu and get a choice. 
+cout << "\t\tMenu Options\n\n";
+     << "our menu describes 3 different concepts and can tell you a users birth year odd or even, user weekly income tax, pie slice count.\n";
+     << "1. asking user for age and determining if birth year is odd or even\n";
+     << "2. calculating user weekly income tax\n";
+     << "3. calculating pie slice count\n";
+     << "4. quit the program\n\n";
+     << "Please enter an option 1 to 4:? \n";
+cin  >> choice;
+    
+//input validation
+
+
+
+
+
+
+}
+//Get keyboard ready to get input from the user because user is the one who's going to tell which one of these they're going to be choosing.
+//Depending on which one of these we have case statements for every individual item. 
+//Start by saying switch and the choice that we have gotten froom the user is going to now be the one that we are going to use to find out out which one of these cases we're going to go to and excute.
+//In this case we have case 1, 2, 3, and 4.
+// add: If I don't input a  break statement, the program will fall through and execute every single case.
